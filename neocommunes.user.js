2104d93272c5b037e906d56d5efe87ebaf1342de
theo-baccam <theo.baccam@laplateforme.io>
2024-11-25 23:34:22 +0100

    feat: if user is at homepage and not connected, don't run

--- neocommunes.user.js
+++ neocommunes.user.js
@@ -3,14 +3,31 @@
 // @namespace   https://rotbrain.neocties.org
 // @match       https://neocities.org/
 // @grant       none
-// @version     1.0
+// @version     0.1
 // @author      rotbrain
 // @description A userscript that hides Neocities' social-media features
 // @license     GPL v3
 // ==/UserScript==
 
-function test() {
-    console.log("it works im gonna cry");
+
+function isUserHome() {
+    const homeURL = "https://neocities.org/";
+    const currentURL = window.location.href;
+
+    return homeURL == currentURL;
 }
 
-test();
+function isUserConnected() {
+    const statusNav = document.getElementsByClassName("status-Nav")[0];
+    const dropdown = statusNav.getElementsByClassName("dropdown")[0];
+
+    return dropdown != undefined;
+}
+
+
+// The main function
+(function() {
+    if (isUserHome() && !isUserConnected()) {
+        return;
+    };
+})();
